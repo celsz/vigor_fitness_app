c284fef313c90408115ac3d410af3edb9444a99c
celsz <none@none.ru>
2025-07-30 13:52:06 +0300

    add dark theme support

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
       ),
+      darkTheme: ThemeData.dark(),
       home: const MyHomePage(title: 'Vigor'),
     );
   }
--- lib/widgets/training_card.dart
+++ lib/widgets/training_card.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/material.dart';
 
-class TrainingCard extends StatelessWidget{
+class TrainingCard extends StatelessWidget {
   final String name;
   final String type;
   final String repeats;
   final String time;
 
-  const TrainingCard({super.key,
-  required this.name,
-  required this.type,
-  required this.repeats,
-  required this.time});
+  const TrainingCard({
+    super.key,
+    required this.name,
+    required this.type,
+    required this.repeats,
+    required this.time,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Color.fromARGB(255, 195, 207, 207),
+      color:
+      Theme.of(context).brightness == Brightness.dark
+          ? Color.fromARGB(255, 50, 53, 53)
+          : Color.fromARGB(255, 195, 207, 207),
       child: InkWell(
-        onTap: () {
-
-        },
+        onTap: () {},
         child: Container(
           padding: EdgeInsets.only(left: 16, bottom: 10, top: 10),
           child: Column(
             children: [
               Text("Упражнение на "),
-              Text(type, style: TextStyle(fontSize: 25),),
+              Text(type, style: TextStyle(fontSize: 25)),
               Row(
                 children: [
-                  Text(name, style: TextStyle(fontSize: 20),),
-                  VerticalDivider(width: 10,),
-                  Padding(padding: EdgeInsets.only(top: 3), child: Text(repeats, style: TextStyle(fontSize: 12),),),
-                  Padding(padding: EdgeInsets.only(top: 2),child: Text("раз(-а)", style: TextStyle(fontSize: 12),),),
-                  Padding(padding: EdgeInsets.only(right: 10), child: Text(time),)
+                  Text(name, style: TextStyle(fontSize: 20)),
+                  VerticalDivider(width: 10),
+                  Padding(
+                    padding: EdgeInsets.only(top: 3),
+                    child: Text(repeats, style: TextStyle(fontSize: 12)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 2),
+                    child: Text("раз(-а)", style: TextStyle(fontSize: 12)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Text(time),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
-        ),),
+        ),
+      ),
     );
   }
 }
